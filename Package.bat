1036ab8bbce39ff061c48477f66b2709f0d14576
dizaknz <24260789+dizaknz@users.noreply.github.com>
2025-11-15 11:56:01 +1300

    Fix package script for demo

--- Package.bat
+++ Package.bat
@@ -1,7 +1,7 @@
-ECHO on
-for /f "delims=|" %%x in (%CD%\.udir) do set UEDir=%%x
-IF "%UEDir" == "" (
-	ECHO "Error: run build first, no directory found for Unreal Engine"
+ECHO ON
+for /f "delims=|" %%x in (.uedir) do set UEDir=%%x
+IF "%UEDir%" == "" (
+	ECHO "Error: no directory found for Unreal Engine"
 	EXIT /b
 )
 SET ClientConfig=Development
@@ -13,5 +13,5 @@ IF "%1" == "" (
 SHIFT
 
 ECHO "Packaging UEBox"
-"%UEDIR%"\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -unattended -utf8output -nocompile -cook -stage -package -prereqs -pak -nop4 -unrealexe=UE4Editor-Cmd -clientconfig=%ClientConfig% -platform=Win64 -project="%CD%\UEBox.uproject" -archive -archivedirectory="%CD%\Packaged" -createreleaseversion=0.0.1 -map=/Game/Maps/RoomLevel
+"%UEDir%"\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -unattended -utf8output -nocompile -cook -stage -package -prereqs -pak -nop4 -unrealexe=UnrealEditor-Cmd.exe -clientconfig=%ClientConfig% -platform=Win64 -project="%CD%\UEBox.uproject" -archive -archivedirectory="%CD%\Packaged" -createreleaseversion=0.0.1 -map=/Game/Maps/RoomLevel
 
