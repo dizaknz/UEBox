65be3aa3826c0bb003c54f9f79320b2374ad97b7
dizaknz <24260789+dizaknz@users.noreply.github.com>
2025-11-17 21:21:10 +1300

    Formatting fixes to code

--- Plugins/NavRoute/Source/NavRoute/Private/GridRoute/WorldGrid.cpp
+++ Plugins/NavRoute/Source/NavRoute/Private/GridRoute/WorldGrid.cpp
@@ -1,32 +1,32 @@
 #include "GridRoute/WorldGrid.h"
 
-bool AWorldGrid::GetGridCellForWorldPos(const FVector& worldPos, FIntPoint& gridPos) const
+bool AWorldGrid::GetGridCellForWorldPos(const FVector& WorldPos, FIntPoint& GridPos) const
 {
-	FVector myLoc = GetActorLocation();
+    FVector Loc = GetActorLocation();
 
-	gridPos.Y = (worldPos.X - myLoc.X) / CellSize.X;
-	gridPos.X = (worldPos.Y - myLoc.Y) / CellSize.Y;
+    GridPos.Y = (WorldPos.X - Loc.X) / CellSize.X;
+    GridPos.X = (WorldPos.Y - Loc.Y) / CellSize.Y;
 
-	return (gridPos.X >= 0 && gridPos.Y >= 0 && gridPos.X < MapSize.X && gridPos.Y < MapSize.Y);
+    return (GridPos.X >= 0 && GridPos.Y >= 0 && GridPos.X < MapSize.X && GridPos.Y < MapSize.Y);
 }
 
-FVector AWorldGrid::GetWorldPosForGridCell(const FIntPoint& pos) const
+FVector AWorldGrid::GetWorldPosForGridCell(const FIntPoint& Pos) const
 {
-	FVector myLoc = GetActorLocation();
-	return FVector(pos.Y * CellSize.X + myLoc.X, pos.X * CellSize.Y + myLoc.Y, myLoc.Z);
+    FVector Loc = GetActorLocation();
+    return FVector(Pos.Y * CellSize.X + Loc.X, Pos.X * CellSize.Y + Loc.Y, Loc.Z);
 }
 
-FVector AWorldGrid::GetWorldPosForGridCellCentre(const FIntPoint& pos) const
+FVector AWorldGrid::GetWorldPosForGridCellCentre(const FIntPoint& Pos) const
 {
-	return GetWorldPosForGridCell(pos) + (FVector(CellSize.Y, CellSize.X, 0) * 0.5f);
+    return GetWorldPosForGridCell(Pos) + (FVector(CellSize.Y, CellSize.X, 0) * 0.5f);
 }
 
 bool AWorldGrid::IsValidGridCell(const FIntPoint& Location) const
 {
-	return (Location.X >= 0 && Location.Y >= 0 && Location.X < MapSize.X && Location.Y < MapSize.Y);
+    return (Location.X >= 0 && Location.Y >= 0 && Location.X < MapSize.X && Location.Y < MapSize.Y);
 }
 
 bool AWorldGrid::IsGridCellWalkable(const FIntPoint& Location) const
 {
-	return (!Cells.Contains(Location));
+    return (!Cells.Contains(Location));
 }
--- Plugins/NavRoute/Source/NavRoute/Public/GridRoute/WorldGridNavigationData.h
+++ Plugins/NavRoute/Source/NavRoute/Public/GridRoute/WorldGridNavigationData.h
@@ -11,20 +11,21 @@ class AWorldGrid;
 UCLASS(config = Engine, defaultconfig, hidecategories = (Input, Rendering, Tags, "Utilities|Transformation", Actor, Layers, Replication), notplaceable)
 class NAVROUTE_API AWorldGridNavigationData : public ANavigationData
 {
-	GENERATED_BODY()
-
-	AWorldGridNavigationData(const FObjectInitializer& ObjectInitializer);
-
-	virtual void BeginPlay() override;
+    GENERATED_BODY()
 
 public:
-	static FPathFindingResult FindPath(const FNavAgentProperties& AgentProperties, const FPathFindingQuery& Query);
+    AWorldGridNavigationData(const FObjectInitializer& ObjectInitializer);
 
-	bool ProjectPoint(const FVector& Point, FNavLocation& OutLocation, const FVector& Extent, FSharedConstNavQueryFilter Filter = NULL, const UObject* Querier = NULL) const override;
+    virtual void BeginPlay() override;
+
+    static FPathFindingResult FindPath(const FNavAgentProperties& AgentProperties, const FPathFindingQuery& Query);
+
+    bool ProjectPoint(const FVector& Point, FNavLocation& OutLocation, const FVector& Extent, FSharedConstNavQueryFilter Filter = NULL, const UObject* Querier = NULL) const override;
 
 private:
-	AWorldGrid* WorldGridActor;
+    UPROPERTY()
+    AWorldGrid* WorldGridActor;
 
-	WorldGridGraph Graph;
-	FGraphAStar<WorldGridGraph>* Pathfinder;
+    WorldGridGraph Graph;
+    FGraphAStar<WorldGridGraph>* Pathfinder;
 };
